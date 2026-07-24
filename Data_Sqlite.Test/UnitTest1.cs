acc0f02f00dd013bd700bebd229d03488634f379
Matthew <nexx42tv@gmail.com>
2026-07-24 15:36:15 +0100

    add tests

---
+++ Data_Sqlite.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Data_Sqlite.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Logic.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Logic.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
